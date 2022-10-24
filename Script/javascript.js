713385017cc52b66042853210aed3c323dcea0ef
AngeloDiMortimer <angelodimortimer99@gmail.com>
2022-10-24 16:51:22 -0400

    Edited javascript file

--- Script/javascript.js
+++ Script/javascript.js
@@ -1,7 +1,17 @@
 const array = ["Rock", "Paper", "Scissors"];
+let playerScore = 0;
+let computerScore = 0;
+
+let playerChoice = (string) => string.charAt(0).toUpperCase() + string.slice(1).toLowerCase(); // gets user string and capitalize only the first letter
 
 let getComputerChoice = (arr) => { //Returns an index from an array at random
     return arr[~~(Math.random() * arr.length)];
 };
 
-console.log(getComputerChoice(array));
+
+
+let playRound = (computerSelection, playerSelection) => {
+    computerSelection = getComputerChoice(array);
+    playerSelection = playerChoice(prompt("Please choice Rock, Paper, or Scissors: "));
+    return playerSelection;
+};
