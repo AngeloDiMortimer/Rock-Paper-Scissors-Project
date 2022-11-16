533e2b2f5076109f241c6deb46f4acd32131ed38
AngeloDiMortimer <angelodimortimer99@gmail.com>
2022-11-16 17:40:18 -0400

    Edited index.html and javascript.js files

--- Script/javascript.js
+++ Script/javascript.js
@@ -5,7 +5,11 @@ const rockButton = document.querySelector('#rock');
 const paperButton = document.querySelector('#paper');
 const scissorsButton = document.querySelector('#scissors');
 
+const playerScoreSpan = document.querySelector('.player-score');
+const computerScoreSpan = document.querySelector('.computer-score');
+
 const outcomeDiv = document.querySelector('#outcome');
+const p = document.createElement('p');
 
 let computerPlay = () => { //gets the choice of the computer
     const array = ["Rock", "Paper", "Scissors"];
@@ -15,15 +19,18 @@ let computerPlay = () => { //gets the choice of the computer
 
 
 let playRound = (computerSelection, playerSelection) => { //Plays a round of the game
+
     if (computerSelection == playerSelection) { //It's a tie
-        return `It's a tie!\nYou both picked ${playerSelection}\nPlayer Score:${playerScore} Computer Score: ${computerScore}`
+
+        p.innerText = `It's a tie! You both picked ${playerSelection}`;
+
     } else if ( //Player loses
         (computerSelection == "Rock" && playerSelection == "Scissors") ||
         (computerSelection == "Scissors" && playerSelection == "Paper") ||
         (computerSelection == "Paper" && playerSelection == "Rock") 
     ) {
         computerScore++;
-        return `You Lose!\n${computerSelection} beats ${playerSelection}\nPlayer Score:${playerScore} Computer Score: ${computerScore}`
+        p.innerText = `You Lose!\n${computerSelection} beats ${playerSelection}`;
     } else if ( //Player wins
         (playerSelection == "Rock" && computerSelection == "Scissors") ||
         (playerSelection == "Scissors" && computerSelection == "Paper") ||
@@ -31,9 +38,30 @@ let playRound = (computerSelection, playerSelection) => { //Plays a round of the
     )
     {
         playerScore++;
-        return `You win!\n${playerSelection} beats ${computerSelection}!\nPlayer Score:${playerScore} Computer Score: ${computerScore}`
+        p.innerText = `You win!\n${playerSelection} beats ${computerSelection}!`;
     } 
+    outcomeDiv.appendChild(p);
     
+};
+
+const checkForWinner = (playerScore, computerScore) => { //Chequea quien fue el ganador de la ronda
+    const h2 = document.createElement('h2');
+
+    if (playerScore === 5) {
+        h2.classList.add('player-won');
+        h2.innerText = `You won! ${playerScore} to ${computerScore} great job beating the computer!`
+    }
+    if (computerScore === 5) {
+        h2.classList.add('computer-won');
+        h2.innerText = `You lost! ${playerScore} to ${computerScore} great job at losing to the computer!`
+    }
+    outcomeDiv.appendChild(h2);
+    
+};
+
+const updateScores = (playerScore, computerScore) => {
+    playerScoreSpan.innerText = `Player Score: ${playerScore}`;
+    computerScoreSpan.innerText = `Player Score: ${computerScore}`;
 };
 
 //Evento cuando se pulsan los botones
@@ -41,16 +69,22 @@ rockButton.addEventListener('click', () => {
     const computerSelection = computerPlay();
     const playerSelection = 'Rock';
     playRound(computerSelection, playerSelection);
+    updateScores(playerScore, computerScore);
+    checkForWinner(playerScore, computerScore);
 });
 
 paperButton.addEventListener('click', () => {
     const computerSelection = computerPlay();
     const playerSelection = 'Paper';
     playRound(computerSelection, playerSelection);
+    updateScores(playerScore, computerScore);
+    checkForWinner(playerScore, computerScore);
 });
 
 scissorsButton.addEventListener('click', () => {
     const computerSelection = computerPlay();
     const playerSelection = 'Scissors';
     playRound(computerSelection, playerSelection);
+    updateScores(playerScore, computerScore);
+    checkForWinner(playerScore, computerScore);
 });
